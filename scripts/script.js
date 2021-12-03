e35393da1c400a310a3992c6526d9ea01c50c232
reneestapert <94540366+reneestapert@users.noreply.github.com>
2021-12-03 15:34:50 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
-// JavaScript Docum ent
+// JavaScript Document
+
+
+
+var hamMenu = document.querySelector ("header nav:first-of-type ul li:nth-of-type(1)");
+
+hamMenu.addEventListener("click", uitschuivenMenu);
+
+function uitschuivenMenu (){
+    var Menu = document.querySelector ("header nav:nth-of-type(2)");
+
+    console.log("klik");
+    Menu.classList.toggle ("uitschuiven");
+
+};
+
+
+
+
+
